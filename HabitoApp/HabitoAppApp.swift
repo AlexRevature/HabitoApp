d1645d84fde2053a6e4d69b1e15201c8dd4e638d
Alex Cabrera <alcamiz@icloud.com>
2025-02-04 14:23:32 -0700

    Initial Commit

---
+++ HabitoApp/HabitoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoAppApp.swift
+//  HabitoApp
+//
+//  Created by Alex Cabrera on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
