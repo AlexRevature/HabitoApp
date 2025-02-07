ff9b62c0765d2e365601cce6321db0eb67740991
Alex Cabrera <alcamiz@icloud.com>
2025-02-07 08:06:41 -0700

    Habit main view partially completed - The components are now layed out and working, but still lack proper customization.

--- HabitoApp/View/Habits/HabitMainView.swift
+++ HabitoApp/View/Habits/HabitMainView.swift
@@ -9,10 +9,73 @@ import SwiftUI
 
 struct HabitMainView: View {
     @State private var favoriteColor = 0
+    var selectedItem = 1
+
     var body: some View {
         VStack {
-            
+            CustomPicker(sources: [1,2,3,4,5], selection: selectedItem) { item in
+                VStack {
+                    Text("Thu")
+                    Text("15")
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top, 10)
+            HabitListView()
+                .padding(.horizontal, 8)
+
+
         }
+    }
+}
+
+struct CustomPicker<Data, Content>: View where Data: Hashable, Content: View {
+
+    let sources: [Data]
+    @State var selection: Data?
+    let itemBuilder: (Data) -> Content
+    var backgroundColor = Color.init(uiColor: .customSecondary)
+
+    init(sources: [Data], selection: Data?, @ViewBuilder itemBuilder: @escaping (Data) -> Content) {
+
+        self.sources = sources
+        self.selection = selection
+        self.itemBuilder = itemBuilder
+    }
+
+    var body: some View {
+        ZStack(alignment: .center) {
+            HStack {
+                ForEach(sources, id: \.self) { item in
+                    itemBuilder(item)
+                        .foregroundColor(selection == item ? .white : nil)
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 15)
+                        .background {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(selection == item ? Color.init(uiColor: .customPrimary) : backgroundColor)
+                        }
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 0.150)) {
+                                selection = item
+                            }
+                        }
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .background(
+                RoundedRectangle(cornerRadius: 6.0)
+                    .fill(backgroundColor)
+            )
+
+        }
+    }
+
+    func setBackgroundColor(color: Color) ->  CustomPicker {
+        var returnView = self // Value type, copied
+        returnView.backgroundColor = color
+        return returnView
     }
 }
 
