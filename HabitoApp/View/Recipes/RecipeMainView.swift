7ccc55fdc78f7ad51734779ada81fa32131d8821
Areeb Durrani <areeb702@revature.net>
2025-02-11 07:46:58 -0800

    move code to main view

--- HabitoApp/View/Recipes/RecipeMainView.swift
+++ HabitoApp/View/Recipes/RecipeMainView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct RecipeMainView: View {
+
+    var image: UIImage
+    var recipes = RecipeModel.shared.getRecipes()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Healthy Eating")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: 120, alignment: .leading)
+                Spacer()
+            }
+            .padding(.init(top: 50, leading: 25, bottom: 50, trailing: 25))
+            .background {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .opacity(0.9)
+                    .background(.green)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            List(recipes, id: \.id){
+                recipe in NavigationLink(destination: RecipeListView(recipe: recipe)){
+                    VStack{
+                        Image(uiImage: UIImage(named: "mainscene")!)
+                        Text(recipe.title).foregroundColor(.primary)
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    RecipeMainView()
+    RecipeMainView(image: UIImage(named: "sample")!)
 }
