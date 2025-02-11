a19f97bfce649ccdf4ce814779ffd1f094c41dfe
Areeb Durrani <areeb702@revature.net>
2025-02-11 07:48:27 -0800

    simplify main view

--- HabitoApp/View/Recipes/RecipeMainView.swift
+++ HabitoApp/View/Recipes/RecipeMainView.swift
@@ -8,28 +8,9 @@
 import SwiftUI
 
 struct RecipeMainView: View {
-
-    var image: UIImage
     var recipes = RecipeModel.shared.getRecipes()
     var body: some View {
-        VStack {
-            HStack {
-                Text("Healthy Eating")
-                    .font(.title)
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: 120, alignment: .leading)
-                Spacer()
-            }
-            .padding(.init(top: 50, leading: 25, bottom: 50, trailing: 25))
-            .background {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-                    .opacity(0.9)
-                    .background(.green)
-            }
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            List(recipes, id: \.id){
+        List(recipes, id: \.id){
                 recipe in NavigationLink(destination: RecipeListView(recipe: recipe)){
                     VStack{
                         Image(uiImage: UIImage(named: "mainscene")!)
@@ -39,8 +20,8 @@ struct RecipeMainView: View {
             }
         }
     }
-}
+
 
 #Preview {
-    RecipeMainView(image: UIImage(named: "sample")!)
+    RecipeMainView()
 }
