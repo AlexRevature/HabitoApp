bc67c1844e0e0e611eaf2e2a2c8f865041d63f81
Alex Cabrera <alcamiz@icloud.com>
2025-02-13 12:41:18 -0700

    Add RootView from previous commit - Didn't include it in git accidentally

---
+++ HabitoApp/RootView.swift
@@ -0,0 +1,34 @@
+//
+//  RootView.swift
+//  HabitoApp
+//
+//  Created by Alex Cabrera on 2/13/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+
+    @Environment(AccountViewModel.self) var viewModel
+
+    var body: some View {
+        NavigationStack {
+            if viewModel.currentUser == nil {
+                SigninView()
+            } else {
+                CustomTabView()
+            }
+        }
+        .animation(.linear(duration: 1), value: viewModel.currentUser)
+    }
+}
+
+#Preview {
+
+    let viewModel = AccountViewModel()
+
+    try? KeychainManager.deleteCredentials()
+    _ = try? viewModel.createUser(username: "test", email: "test@test.com", phone: "1236540987", password: "password1#", passwordVerify: "password1#")
+
+    return RootView().environment(viewModel)
+}
